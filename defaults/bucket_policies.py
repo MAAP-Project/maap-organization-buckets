8e3ef50e223d377edd883248c1451db29483862a
Jamison French <jamison@developmentseed.org>
2026-03-11 11:54:35 -0500

    chore: modify default bucket permissions

--- defaults/bucket_policies.py
+++ defaults/bucket_policies.py
@@ -26,13 +26,18 @@ def apply_default_bucket_policy(
                 actions=[
                     "s3:GetBucketLocation",
                     "s3:GetObject",
-                    "s3:GetObjectVersion",
                     "s3:GetObjectTagging",
-                    "s3:GetObjectVersionTagging",
+                    "s3:GetObjectAttributes",
                     "s3:ListBucket",
-                    "s3:ListBucketMultipartUploads",
                     "s3:ListBucketVersions",
                     "s3:ListMultipartUploadParts",
+                    "s3:ListBucketMultipartUploads",
+                    "s3:PutObject",
+                    "s3:PutObjectAcl",
+                    "s3:PutObjectTagging",
+                    "s3:DeleteObject",
+                    "s3:AbortMultipartUpload",
+                    "s3:RestoreObject"
                 ],
                 principals=[
                     iam.ArnPrincipal(role_arn) for role_arn in maap_api_role_arns
